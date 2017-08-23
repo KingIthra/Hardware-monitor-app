03bf3ad014d587820783b7ac3bad931ebbd4c6dc
mrdck <mrdck@icloud.com>
2017-08-23 14:18:35 +0200

    OpenHardwareMonitor.dll , dispatchtimer  , PerformanceCounter for CPU and Memory usage

---
+++ README.md
@@ -0,0 +1,15 @@
+### About project
+
+
+Following app let user see usage of CPU , RAM and CPU Temp with use of OpenHardwareMonitor.dll library and C# .NET framework with WPF.
+
+
+![screenshot1](screenshots/screenshot1.png)
+
+
+
+### Features
+
+* Shows CPU usage
+* Show Memory usage in Mb
+* Show CPU Temp. Note : Not all sensors are supported by WMI(Windows Management Instrumentation)
--- TempsApp/MainWindow.xaml.cs
+++ TempsApp/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Management;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -13,6 +14,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.Timers;
+using System.Management;
+using Timer = System.Threading.Timer;
+using System.Speech.Synthesis;
+using System.Threading;
+using OpenHardwareMonitor;
+using OpenHardwareMonitor.Collections;
+using OpenHardwareMonitor.Hardware;
 
 namespace TempsApp
 {
@@ -21,9 +30,73 @@ namespace TempsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+
+
+        public float temperature;
+
         public MainWindow()
         {
+           
+
             InitializeComponent();
+
+
+            var dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(5000);
+            dispatcherTimer.Start();
+
+           
+
+
+
+
+
+
+
+
         }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            PerformanceCounter memPref = new PerformanceCounter("Memory", "Available MBytes");
+            PerformanceCounter cpuPerf = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            var computer = new Computer();
+            computer.CPUEnabled = true;
+            computer.Open();
+            
+
+            foreach (var hardware in computer.Hardware)
+            {
+                if (hardware.HardwareType == HardwareType.CPU)
+                {
+                    hardware.Update();
+                    foreach (var sensor in hardware.Sensors)
+                    {
+                       
+                        if (sensor.SensorType == SensorType.Temperature)
+                        temperature = sensor.Value.GetValueOrDefault();
+                       
+                    }
+                }
+            }
+
+            temp.Content = temperature;
+            cpuUsage.Content = (int)cpuPerf.NextValue() + "%";
+            Thread.Sleep(100);
+            temp.Content = temperature;
+            cpuUsage.Content = (int) cpuPerf.NextValue() + "%";
+           ramUsage.Content = memPref.NextValue() + "Mb";
+
+
+
+
+
+        }
+
+
+
     }
 }
